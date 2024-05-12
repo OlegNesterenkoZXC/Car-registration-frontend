beb2b39881332d9fe25d53aa9a42af578bc61815
Oleg <olegik-n@yandex.com>
2024-05-12 17:31:44 +0300

    Добавлена навигационная панель

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+    <v-app-bar color="deep-purple" dark app>
+			<v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+			<v-toolbar-title>Система регистрации автомобилей</v-toolbar-title>
+			<v-spacer></v-spacer>
+		</v-app-bar>
+
+		<v-navigation-drawer v-model="drawer" absolute temporary app>
+			<v-list nav dense>
+				<v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+					<v-list-item to="/">
+						<v-list-item-icon>
+							<v-icon>mdi-home</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>Домой</v-list-item-title>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+		</v-navigation-drawer>
     <v-main>
       <v-overlay v-if="isLoading">
         <v-progress-circular
@@ -63,6 +82,8 @@ export default {
       metaMaskProvider: null,
       contractAddress: '',
       error: null,
+      drawer: false,
+      group: null
     }
   },
   computed: {
