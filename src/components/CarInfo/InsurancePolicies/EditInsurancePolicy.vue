380c4015614be1270c98a4aa8e2ec61217e26fb8
Oleg <olegik-n@yandex.com>
2024-05-26 22:10:18 +0300

    Refactoring

--- src/components/CarInfo/InsurancePolicies/EditInsurancePolicy.vue
+++ src/components/CarInfo/InsurancePolicies/EditInsurancePolicy.vue
@@ -1,91 +1,70 @@
 <template>
-  <CustomModal
-    ref="modal"
-    disabled
+  <EditPanelTemplate
+    ref="editTemplatePanel"
+    v-model="isEditor"
     :loading="isLoading"
-    :title="modalTitle"
-    :confirmButtonText="modalConfirmButtonText"
-    :disabledConfirmButton="modalDisabledConfirmButton"
+    :disabled="isDisabledModal"
+    :modal="modalType"
     @confirm="confirmHandler"
   >
-    <div v-if="metaMaskProvider">
-      <v-card-text>Ваш аккаунт: {{ signer.address }}</v-card-text>
-      
-      <v-card-text>
-        <v-alert
-          v-if="alert"
-          prominent
-          text
-          :type="alert.type"
-        >
-          {{ alert.text }}
-        </v-alert>
+    <v-alert
+      v-if="alert"
+      prominent
+      text
+      :type="alert.type"
+    >
+      {{ alert.text }}
+    </v-alert>
 
-        <v-form
-          ref="form"
-          v-model="isValidForm"
-          :disabled="isDisabledForm"
-        >
-          <v-text-field
-            v-model="series"
-            label="Серия"
-            required
-            :counter="3"
-            :rules="[seriesRules.required, seriesRules.length]"
-          />
-          <v-text-field
-            v-model="number"
-            label="Номер"
-            required
-            :counter="10"
-            :rules="[numberRules.required, numberRules.length]"
-          />
-        </v-form>
-      </v-card-text>
-    </div>
-
-    <ConnectMetaMask
-      v-else
-      ref="connectMetaMask"
-    />
-  </CustomModal>
+    <v-form
+      ref="form"
+      v-model="isValidForm"
+      :disabled="isDisabledForm"
+    >
+      <v-text-field
+        v-model="formInsurancePolicy.series"
+        label="Серия"
+        required
+        :counter="3"
+        :rules="seriesRules"
+      />
+      <v-text-field
+        v-model="formInsurancePolicy.number"
+        label="Номер"
+        required
+        :counter="10"
+        :rules="numberRules"
+      />
+    </v-form>
+  </EditPanelTemplate>
 </template>
 
 <script>
-import ConnectMetaMask from '@/components/ConnectMetaMask.vue'
-import CustomModal from '@/components/elements/CustomModal.vue'
+import EditPanelTemplate from '@/components/CarInfo/EditPanelTemplate.vue'
 
-import { 
-  hasRole as hasRoleAPI, 
+import {
   addInsurancePolicy as addInsurancePolicyAPI,
   editInsurancePolicy as editInsurancePolicyAPI,
   removeInsurancePolicy as removeInsurancePolicyAPI,
 } from '@/libs/api'
-
-import { encodeRole } from '@/libs/utils'
 
 import { MODE, EDITOR_ROLE, ALERT_TYPE } from '@/constants';
 import { mapState } from 'vuex';
 
 const MODAL_TYPE_MAPPING = {
   [MODE.ADD]: {
-    title: 'Добавление полиса',
+    title: 'Добавление полиса ТС',
     confirmButton: 'Добавить',
     disabledForm: false,
   },
   [MODE.EDIT]: {
-    title: 'Редактирование полиса',
+    title: 'Редактирование полиса ТС',
     confirmButton: 'Изменить',
     disabledForm: false,
   },
   [MODE.REMOVE]: {
-    title: 'Удаление полиса',
+    title: 'Удаление полиса ТС',
     confirmButton: 'Удалить',
-    disabledForm: true,
-  },
-  CONNECT_METAMASK: {
-    title: 'Подключение к MetaMask',
-    confirmButton: 'Подключиться',
     disabledForm: true,
   }
 }
@@ -105,49 +84,41 @@ const POLICY_ALERT_MAPPING = {
   }
 }
 
+const SERIES_RULES = {
+  required: (v) => !!v || 'Серия обязательное поле',
+  length: (v) => !v || v.length === 3 || 'Неправильная длина'
+}
+const NUMBER_RULES = {
+  required: (v) => !!v || 'Номер обязательное поле',
+  length: (v) => !v || v.length === 10 || 'Неправильная длина'
+}
+
 export default {
-  components: { CustomModal, ConnectMetaMask },
+  components: { EditPanelTemplate },
   props: {
     mode: {
+      type: String,
+      required: true
+    },
+    vin: {
       type: String,
       required: true
     },
     insurancePolicy: {
       type: Object,
       default: null
-    },
-    vin: {
-      type: String,
-      required: true
     }
   },
   data () {
     return {
-      isLoading: false,
-      isDisabled: false,
       isEditor: false,
       isValidForm: false,
-      series: '',
-      seriesRules: {
-        required: (v) => !!v || 'Серия обязательное поле',
-        length: (v) => !v || v.length === 3 || 'Неправильная длина',
-      },
-      number: '',
-      numberRules: {
-        required: (v) => !!v || 'Номер обязательное поле',
-        length: (v) => !v || v.length === 10 || 'Неправильная длина',
+      isLoading: false,
+      formInsurancePolicy: {
+        series: '',
+        number: ''
       },
       alert: null
-    }
-  },
-  watch: {
-    addressSigner: {
-      immediate: true,
-      handler (value, oldValue) {
-        if (value && value !== oldValue) {
-          this.refreshSigner()
-        }
-      }
     }
   },
   computed: {
@@ -158,129 +129,73 @@ export default {
       abi: 'abi',
       contractAddress: 'contractAddress'
     }),
-    addressSigner () {
-      return this.signer?.address ?? ''
-    },
     modalType () {
-      if (!this.metaMaskProvider) {
-        return MODAL_TYPE_MAPPING.CONNECT_METAMASK
-      }
-
       return MODAL_TYPE_MAPPING[this.mode]
     },
-    modalTitle () {
-      return this.modalType.title
-    },
-    modalConfirmButtonText () {
-      return this.modalType.confirmButton
-    },
-    modalDisabledConfirmButton () {
-      if (!this.metaMaskProvider || this.mode === MODE.REMOVE) {
-        return false
-      }
-
-      if (!this.isEditor) {
-        return true
-      }
-
-      if (this.mode === MODE.EDIT) {
-        return !this.isValidForm || !this.isDataChanged
-      }
-
-      return !this.isValidForm
-    },
     isDisabledForm () {
-      return !this.isEditor || MODAL_TYPE_MAPPING[this.mode].disabledForm
+      return !this.isEditor || this.isLoading || this.modalType.disabledForm
+    },
+    isDisabledModal () {
+      const isDataChanged = (this.mode === MODE.EDIT ? this.isDataChanged : true)
+
+      return !this.isEditor || !this.isValidForm || !isDataChanged
+    },
+    seriesRules () {
+      return [SERIES_RULES.required, SERIES_RULES.length]
+    },
+    numberRules () {
+      return [NUMBER_RULES.required, NUMBER_RULES.length]
     },
     isDataChanged () {
-      return this.insurancePolicy.series !== this.series || this.insurancePolicy.number !== this.number
+      return this.insurancePolicy.series !== this.formInsurancePolicy.series || 
+        this.insurancePolicy.number !== this.formInsurancePolicy.number
     }
   },
   methods: {
-    confirmHandler () {
-      if (!this.metaMaskProvider) {
-        this.initMetMaskProvider()
-
-        return
-      }
-
-      this.startEditData()
-    },
     openDialog () {
       this.alert = null
-      this.$refs.modal.openDialog()
+      this.$refs.editTemplatePanel.openDialog()
 
       this.$nextTick(this.initFormData)      
+    },
+    confirmHandler () {
+      let method = null
+      if (this.mode === MODE.ADD) {
+        method = this.addInsurancePolicy
+      } else if (this.mode === MODE.EDIT) {
+        method = this.editInsurancePolicy
+      } else {
+        method = this.removeInsurancePolicy
+      }
+
+      const alert = POLICY_ALERT_MAPPING[this.mode]
+
+      this.startEditData(method, alert)
     },
     initFormData () {
       if (this.mode === MODE.ADD) {
         this.$refs.form?.reset()
       } else {
-        this.series = this.insurancePolicy.series
-        this.number = this.insurancePolicy.number
+        this.formInsurancePolicy.series = this.insurancePolicy.series
+        this.formInsurancePolicy.number = this.insurancePolicy.number
       }
     },
-    async initMetMaskProvider () {
+    async startEditData (method, alert) {
       this.isLoading = true
-      
-      await this.$refs.connectMetaMask?.initMetMaskProvider()
-
-      this.isLoading = false
-    },
-    async refreshSigner () {
-      try {
-        const hasRole = await this.hasEditorRole()
-        if(!hasRole) {
-          this.alert = ALERT_TYPE.NO_PERMISSION
-
-          this.isEditor = false
-
-          return
-        }
-
-        this.isEditor = true
-      } catch (error) {
-        this.alert = ALERT_TYPE.ACCOUNT_PERMISSION_ERROR
-      }
-
-      this.alert = null
-    },
-    async startEditData () {
-      this.isLoading = true
-      this.isDisabled = true
-      this.alert = null
+      this.alert =  ALERT_TYPE.CONFIRM_TRANSACTION
 
       try {
-        this.alert = ALERT_TYPE.CONFIRM_TRANSACTION
 
-        if (this.mode === MODE.ADD) {
-          await this.addInsurancePolicy()
-        } else if (this.mode === MODE.EDIT) {
-          await this.editInsurancePolicy()
-        } else if (this.mode === MODE.REMOVE) {
-          await this.removeInsurancePolicy()
-        }
+        await method()
 
         this.$emit('success')
       } catch (error) {
         console.error(error)
 
-        this.alert = POLICY_ALERT_MAPPING[this.mode]
+        this.alert = alert
       } finally {
         this.isLoading = false
-        this.isDisabled = false
       }
-    },
-    async hasEditorRole () {
-      const params = {
-        address: this.contractAddress,
-        abi: this.abi,
-        provider: this.httpProvider,
-        signer: this.signer,
-        role: encodeRole(EDITOR_ROLE)
-      }
-
-      return hasRoleAPI(params)
     },
     async addInsurancePolicy () {
       const params = {
@@ -289,8 +204,8 @@ export default {
         provider: this.httpProvider,
         signer: this.signer,
         vin: this.vin,
-        series: this.series,
-        number: this.number
+        series: this.formInsurancePolicy.series,
+        number: this.formInsurancePolicy.number
       }
 
       return addInsurancePolicyAPI(params)
@@ -303,8 +218,8 @@ export default {
         signer: this.signer,
         vin: this.vin,
         index: this.insurancePolicy.index,
-        series: this.series,
-        number: this.number
+        series: this.formInsurancePolicy.series,
+        number: this.formInsurancePolicy.number
       }
 
       return editInsurancePolicyAPI(params)
--- src/components/CarInfo/VehiclePassports/EditVehiclePassport.vue
+++ src/components/CarInfo/VehiclePassports/EditVehiclePassport.vue
@@ -169,6 +169,7 @@ export default {
   },
   methods: {
     openDialog () {
+      this.alert = null
       this.$refs.editTemplatePanel.openDialog()
 
       this.$nextTick(this.initFormData)
